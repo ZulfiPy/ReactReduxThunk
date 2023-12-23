60cae73024662a78b1aaf70d730a43f528f7e025
ZulfiPy <zulfizupy@gmail.com>
2023-12-23 11:14:42 +0200

    App.jsx pages routed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import CustomerPage from "./pages/CustomersPage";
 import VehiclesPage from "./pages/VehiclesPage";
+import DiscountPage from "./pages/DiscountPage";
+import InsurancePage from "./pages/InsurancePage";
+import TicketsPage from "./pages/TicketsPage";
+import AccidentsPage from "./pages/AccidentsPage";
+import TasksPage from "./pages/TasksPage";
+import CRMPage from "./pages/CRMPage";
+import DebtorPage from "./pages/DebtorPage";
 import Missing from "./pages/Missing";
 import { Routes, Route } from "react-router-dom";
 
@@ -15,6 +22,14 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}/>
           <Route path="customers" element={<CustomerPage />}/>
+          <Route path="vehicles" element={<VehiclesPage />}/>
+          <Route path="discount" element={<DiscountPage />}/>
+          <Route path="insurance" element={<InsurancePage />}/>
+          <Route path="tickets" element={<TicketsPage />}/>
+          <Route path="accidents" element={<AccidentsPage />}/>
+          <Route path="tasks" element={<TasksPage />}/>
+          <Route path="crm" element={<CRMPage />}/>
+          <Route path="debtor" element={<DebtorPage />}/>
           <Route path="about" element={<About />}/>
           <Route path="contact" element={<Contact />}/>
           <Route path="*" element={<Missing />}/>
