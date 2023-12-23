83e3fc585a05b9d16b35ed19ea645c9403f7a179
ZulfiPy <zulfizupy@gmail.com>
2023-12-23 16:19:53 +0200

    main.jsx modified

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { fetchCustomers } from './features/customersSlice.jsx';
+import { store } from './app/store.jsx';
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+store.dispatch(fetchCustomers());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />}/>
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />}/>
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>,
 );
--- src/pages/CustomersPage.jsx
+++ src/pages/CustomersPage.jsx
@@ -1,8 +1,24 @@
+import { useSelector } from "react-redux";
+import { getCustomersStatus, getCustomersError } from "../features/customersSlice";
+import CustomerTable from "../components/customersComponents/CustomerTable";
+
 const CustomerModule = () => {
+    const customersStatus = useSelector(getCustomersStatus);
+    const error = useSelector(getCustomersError);
+
+    let content;
+    if (customersStatus === "loading") {
+        content = <p>&quot;Loading...&quot;</p>
+    } else if (customersStatus === "succeeded") {
+        content = <CustomerTable />;
+    } else if (customersStatus === "failed") {
+        content = <p>{error}</p>;
+    }
+
     return (
         <main className="ModuleContent">
-            <h2>Customer Module</h2>
-            <p>Developing in process...</p>
+            <h2>Customers Database Module</h2>
+            {content}
         </main>
     )
 }
