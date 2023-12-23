4403378fcec2f0937f6764668336197510bd5010
ZulfiPy <zulfizupy@gmail.com>
2023-12-23 11:07:56 +0200

    Accidentds page created

---
+++ src/pages/AccidentsPage.jsx
@@ -0,0 +1,10 @@
+const AccidentsPage = () => {
+    return (
+        <main className="ModuleContent">
+            <h2>Accidents Module</h2>
+            <p>Developing in process...</p>
+        </main>
+    )
+}
+
+export default AccidentsPage;
